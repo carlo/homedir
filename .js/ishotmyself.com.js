19c68bf88d00a9b7c9362bc257f0e05c7bcef0df
Carlo Zottmann <carlo@municode.de>
2012-06-05 10:48:43 +0200

    Initial import.

---
+++ .js/ishotmyself.com.js
@@ -0,0 +1,24 @@
+$("a[href^='javascript:popupLandscape']").each( function( i, e ) {
+  var elem = $(e);
+
+  elem.attr( "href", elem.attr("href").replace( /^.*'(.*)'.*$/, '$1' ) );
+});
+
+$("#gallerypage .heading").append(
+  "<a href='#' id='showfullsize' style='float: right'>ISM all</a>"
+);
+
+$("#showfullsize").click( function(evt) {
+  evt.preventDefault();
+
+  var output = "";
+
+  $("a[href*=view_image]").each( function( i, cand ) {
+    if ( match = cand.href.match( /(\/public\/view_image\.php\?[^'$]+)/i ) ) {
+      output += "<img src='http://www.ishotmyself.com" + match[1] + "&m=img' border='1' /><br/><br/>";
+    }
+  });
+
+  $("body").html(output);
+});
+
